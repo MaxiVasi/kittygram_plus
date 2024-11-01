df77e33cfc693aa30dd90a15d576b2e181b15c36
Максим Васильев MaxiVasi <vasiliev.mu@gmail.com>
2024-11-01 22:48:59 +0300

    JWL token

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,5 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken import views
+
+# Для аутентификации по токену.
+# from rest_framework.authtoken import views
 
 from django.urls import include, path
 
@@ -22,5 +24,10 @@ router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token),
+    # Djoser создаст набор необходимых эндпоинтов.
+    # базовые, для управления пользователями в Django:
+    path('auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('auth/', include('djoser.urls.jwt')),
+    # path('api-token-auth/', views.obtain_auth_token),
 ]
