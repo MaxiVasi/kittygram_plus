f57d49876995ecd0f5f1062efaecb957f54abc4f
Максим Васильев MaxiVasi <vasiliev.mu@gmail.com>
2024-11-01 21:58:59 +0300

    before authintification

--- cats/views.py
+++ cats/views.py
@@ -1,9 +1,60 @@
-from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import mixins, viewsets
 
-from .models import Cat
-from .serializers import CatSerializer
+from django.shortcuts import get_object_or_404
+
+from .models import Cat, Owner
+from .serializers import CatSerializer, CatListSerializer, OwnerSerializer
+
+
+# Собираем вьюсет, который будет уметь изменять или удалять отдельный объект.
+# А ничего больше он уметь не будет.
+class UpdateDeleteViewSet(mixins.UpdateModelMixin, mixins.DestroyModelMixin,
+                          viewsets.GenericViewSet):
+    pass
+
+
+class CreateRetrieveViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
+                            viewsets.GenericViewSet):
+    # В теле класса никакой код не нужен! Пустячок, а приятно.
+    pass
+
+
+# С Mixins
+class LightCatViewSet(CreateRetrieveViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+
+    # Использование разных сериализаторов под разные задачи.
+    # Тема Различные сериализаторы для одного вьюсета
+    # для получения списка котиков используется CatListSerializer
+    def get_serializer_class(self):
+        # Если запрошенное действие (action) — получение списка объектов ('list')
+        if self.action == 'list':
+            # ...то применяем CatListSerializer
+            return CatListSerializer
+        # А если запрошенное действие — не 'list', применяем CatSerializer
+        return CatSerializer
+
+    # Тема Нестандартные действия во вьюсетах.
+    # Пишем метод, а в декораторе разрешим работу со списком объектов
+    # и переопределим URL на более презентабельный
+    @action(detail=False, url_path='recent-white-cats')
+    def recent_white_cats(self, request):
+        # Нужно получить записи о пяти котиках белого цвета
+        cats = Cat.objects.filter(color='White')[:5]
+        # Передадим queryset cats сериализатору
+        # и разрешим работу со списком объектов
+        serializer = self.get_serializer(cats, many=True)
+        return Response(serializer.data)
+
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,12 +1,23 @@
-from django.urls import include, path
-
 from rest_framework.routers import DefaultRouter
 
-from cats.views import CatViewSet
+from django.urls import include, path
+
+from cats.views import CatViewSet, LightCatViewSet, OwnerViewSet
 
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
+router.register('owners', OwnerViewSet)
+# Роутер с регулярным выражением.
+router.register(r'mycats', LightCatViewSet)
+
+# При регистрации эндпоинтов с таким URL-префиксом
+# router.register(r'profile/(?P<username>[\w.@+-]+)/', AnyViewSet)
+# ...вьюсет AnyViewSet будет получать на обработку все запросы с адресов
+# /profile/toh@/
+# /profile/nik.nik/
+# /profile/leo/
+# ...и подобных, ограниченных маской регулярного выражения.
 
 urlpatterns = [
     path('', include(router.urls)),
